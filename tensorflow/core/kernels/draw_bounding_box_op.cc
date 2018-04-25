d3f82b1e90b3da7764192f71c2095393f2e142ff
Chew, Wen Xiang <wen.xiang.chew@accenture.com>
2018-04-25 14:57:10 +0800

    missing }

--- tensorflow/core/kernels/draw_bounding_box_op.cc
+++ tensorflow/core/kernels/draw_bounding_box_op.cc
@@ -94,106 +94,106 @@ class DrawBoundingBoxesOp : public OpKernel {
     for (int64 b = 0; b < batch_size; ++b) {
       const int64 num_boxes = boxes.dim_size(1);
       const auto tboxes = boxes.tensor<T, 3>();
-      for (int64 bb = 0; bb < num_boxes; ++bb) {
-        int64 color_index = bb % color_table_length;
-        const int64 min_box_row =
-            static_cast<float>(tboxes(b, bb, 0)) * (height - 1);
-        const int64 min_box_row_clamp =
-            std::max<int64>(min_box_row, 0);
-        const int64 max_box_row =
-            static_cast<float>(tboxes(b, bb, 2)) * (height - 1);
-        const int64 max_box_row_clamp =
-            std::min<int64>(max_box_row, height - 1);
-        const int64 min_box_col =
-            static_cast<float>(tboxes(b, bb, 1)) * (width - 1);
-        const int64 min_box_col_clamp =
-            std::max<int64>(min_box_col, 0);
-        const int64 max_box_col =
-            static_cast<float>(tboxes(b, bb, 3)) * (width - 1);
-        const int64 max_box_col_clamp =
-            std::min<int64>(max_box_col, width - 1);
+	  for (int64 bb = 0; bb < num_boxes; ++bb) {
+		  int64 color_index = bb % color_table_length;
+		  const int64 min_box_row =
+			  static_cast<float>(tboxes(b, bb, 0)) * (height - 1);
+		  const int64 min_box_row_clamp =
+			  std::max<int64>(min_box_row, 0);
+		  const int64 max_box_row =
+			  static_cast<float>(tboxes(b, bb, 2)) * (height - 1);
+		  const int64 max_box_row_clamp =
+			  std::min<int64>(max_box_row, height - 1);
+		  const int64 min_box_col =
+			  static_cast<float>(tboxes(b, bb, 1)) * (width - 1);
+		  const int64 min_box_col_clamp =
+			  std::max<int64>(min_box_col, 0);
+		  const int64 max_box_col =
+			  static_cast<float>(tboxes(b, bb, 3)) * (width - 1);
+		  const int64 max_box_col_clamp =
+			  std::min<int64>(max_box_col, width - 1);
 
-        if (min_box_row > max_box_row || min_box_col > max_box_col) {
-          LOG(WARNING) << "Bounding box (" << min_box_row
-                       << "," << min_box_col
-                       << "," << max_box_row
-                       << "," << max_box_col
-                       << ") is inverted and will not be drawn.";
-          continue;
-        }
-        if (min_box_row >= height || max_box_row < 0 ||
-            min_box_col >= width || max_box_col < 0) {
-          LOG(WARNING) << "Bounding box (" << min_box_row
-                       << "," << min_box_col
-                       << "," << max_box_row
-                       << "," << max_box_col
-                       << ") is completely outside the image"
-                       << " and will not be drawn.";
-          continue;
-        }
+		  if (min_box_row > max_box_row || min_box_col > max_box_col) {
+			  LOG(WARNING) << "Bounding box (" << min_box_row
+				  << "," << min_box_col
+				  << "," << max_box_row
+				  << "," << max_box_col
+				  << ") is inverted and will not be drawn.";
+			  continue;
+		  }
+		  if (min_box_row >= height || max_box_row < 0 ||
+			  min_box_col >= width || max_box_col < 0) {
+			  LOG(WARNING) << "Bounding box (" << min_box_row
+				  << "," << min_box_col
+				  << "," << max_box_row
+				  << "," << max_box_col
+				  << ") is completely outside the image"
+				  << " and will not be drawn.";
+			  continue;
+		  }
 
-        // At this point, {min,max}_box_{row,col}_clamp are inside the
-        // image.
-        CHECK_GE(min_box_row_clamp, 0);
-        CHECK_GE(max_box_row_clamp, 0);
-        CHECK_LT(min_box_row_clamp, height);
-        CHECK_LT(max_box_row_clamp, height);
-        CHECK_GE(min_box_col_clamp, 0);
-        CHECK_GE(max_box_col_clamp, 0);
-        CHECK_LT(min_box_col_clamp, width);
-        CHECK_LT(max_box_col_clamp, width);
+		  // At this point, {min,max}_box_{row,col}_clamp are inside the
+		  // image.
+		  CHECK_GE(min_box_row_clamp, 0);
+		  CHECK_GE(max_box_row_clamp, 0);
+		  CHECK_LT(min_box_row_clamp, height);
+		  CHECK_LT(max_box_row_clamp, height);
+		  CHECK_GE(min_box_col_clamp, 0);
+		  CHECK_GE(max_box_col_clamp, 0);
+		  CHECK_LT(min_box_col_clamp, width);
+		  CHECK_LT(max_box_col_clamp, width);
 
-        // At this point, the min_box_row and min_box_col are either
-        // in the image or above/left of it, and max_box_row and
-        // max_box_col are either in the image or below/right or it.
-        CHECK_LT(min_box_row, height);
-        CHECK_GE(max_box_row, 0);
-        CHECK_LT(min_box_col, width);
-        CHECK_GE(max_box_col, 0);
+		  // At this point, the min_box_row and min_box_col are either
+		  // in the image or above/left of it, and max_box_row and
+		  // max_box_col are either in the image or below/right or it.
+		  CHECK_LT(min_box_row, height);
+		  CHECK_GE(max_box_row, 0);
+		  CHECK_LT(min_box_col, width);
+		  CHECK_GE(max_box_col, 0);
 
-		if (!fill_) {
-			// Draw top line.
-			if (min_box_row >= 0) {
-				for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
-					for (int64 c = 0; c < depth; c++) {
-						canvas(b, min_box_row, j, c) =
-							static_cast<T>(color_table[color_index][c]);
-					}
-			}
-			// Draw bottom line.
-			if (max_box_row < height) {
-				for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
-					for (int64 c = 0; c < depth; c++) {
-						canvas(b, max_box_row, j, c) =
-							static_cast<T>(color_table[color_index][c]);
-					}
-			}
-			// Draw left line.
-			if (min_box_col >= 0) {
-				for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
-					for (int64 c = 0; c < depth; c++) {
-						canvas(b, i, min_box_col, c) =
-							static_cast<T>(color_table[color_index][c]);
-					}
-			}
-			// Draw right line.
-			if (max_box_col < width) {
-				for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
-					for (int64 c = 0; c < depth; c++) {
-						canvas(b, i, max_box_col, c) =
-							static_cast<T>(color_table[color_index][c]);
-					}
-			}
-		}
-		else {
-			for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
-				for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
-					for (int64 c = 0; c < depth; c++) {
-						canvas(b, i, j, c) = static_cast<T>(0);
-					}
+		  if (!fill_) {
+			  // Draw top line.
+			  if (min_box_row >= 0) {
+				  for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
+					  for (int64 c = 0; c < depth; c++) {
+						  canvas(b, min_box_row, j, c) =
+							  static_cast<T>(color_table[color_index][c]);
+					  }
+			  }
+			  // Draw bottom line.
+			  if (max_box_row < height) {
+				  for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
+					  for (int64 c = 0; c < depth; c++) {
+						  canvas(b, max_box_row, j, c) =
+							  static_cast<T>(color_table[color_index][c]);
+					  }
+			  }
+			  // Draw left line.
+			  if (min_box_col >= 0) {
+				  for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
+					  for (int64 c = 0; c < depth; c++) {
+						  canvas(b, i, min_box_col, c) =
+							  static_cast<T>(color_table[color_index][c]);
+					  }
+			  }
+			  // Draw right line.
+			  if (max_box_col < width) {
+				  for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
+					  for (int64 c = 0; c < depth; c++) {
+						  canvas(b, i, max_box_col, c) =
+							  static_cast<T>(color_table[color_index][c]);
+					  }
+			  }
+		  }
+		  else {
+			  for (int64 j = min_box_col_clamp; j <= max_box_col_clamp; ++j)
+				  for (int64 i = min_box_row_clamp; i <= max_box_row_clamp; ++i)
+					  for (int64 c = 0; c < depth; c++) {
+						  canvas(b, i, j, c) = static_cast<T>(0);
+					  }
 
-		}
-		
+		  }
+	  }
     }
   }
 };
